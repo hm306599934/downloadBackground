893dbdbcb96035d300306a71cdad4df74e0d4353
Jesse <2362572612@qq.com>
2014-12-22 11:17:14 +0800

    add test
    add test

--- downloadBackground/HMAppDelegate.m
+++ downloadBackground/HMAppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    NSLog(@"test");
+    NSLog(@"test");//test
     return YES;
 }
 							
